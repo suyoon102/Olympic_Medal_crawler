729f9b509698bce633cadb7a6e79bb5f5adda41e
julia0519aa <julia0519aa@naver.com>
2021-05-11 18:34:50 +0900

    'finish'

--- Olympic_Medal_crawler.py
+++ Olympic_Medal_crawler.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import matplotlib
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
@@ -5,6 +7,49 @@ html = urlopen("https://gsic.sports.or.kr/com/cop/game/game_info.do?partCode=1&g
 
 soup = BeautifulSoup(html, "lxml")
 
-chart_table = soup.find_all('table',{'class':'table_t boardList selection'}) #메달 통계가 적힌 테이블 불러와 변수에 저장
+chart_table = soup.find_all('table',{'class':'table_t boardList selection'})
 
-chart_table_tbody=chart_table[0].find_all("tbody") #순위   국가   금메달   은메달   동메달   합계 라고 되어있는 부분 제외밑 통계부분 불러오기
+chart_table_tbody=chart_table[0].find_all("tbody")
+
+chart_table_tr=chart_table_tbody[0].find_all("tr")
+
+tr_list=[]
+for i in chart_table_tr:
+    td = i.find_all('td')
+    tr_list.append(i.get_text())
+
+rank=[]
+country=[]
+gold=[]
+silver=[]
+bronze=[]
+sum_medal=[]
+for i in tr_list:
+    str=i.split('\n')
+    del str[-1]
+    del str[0]
+    print(str)
+    country.append(str[1])
+    gold.append(int(str[2]))
+    silver.append(int(str[3]))
+    bronze.append(int(str[4]))
+    sum_medal.append(int(str[5]))
+
+print(rank)
+print(country)
+print(gold)
+print(silver)
+print(bronze)
+print(sum_medal)
+
+import numpy as np
+import sympy as sp
+import matplotlib.pyplot as plt
+plt.rc('font',family='Malgun Gothic')
+x=np.arange(30)
+print(x)
+print(gold[:30])
+plt.bar(x,gold[:30])
+plt.xticks(x,country[:30],rotation='vertical')
+plt.show()
+plt.xticks(country)
